05a2cdf828f803a6ba4778c42f7a46c1aad63e61
C5m7b4 <mbedingfield@dcrpos.com>
2022-01-09 15:12:31 -0600

    added terminations

--- src/utils/fns.ts
+++ src/utils/fns.ts
@@ -9,18 +9,18 @@ export function generateToastId(): string {
 export const getToastContainerPosition = (position: ToastPosition) => {
   switch (position) {
     case 'top-right':
-      return 'position: fixed; top: 10px; right: 10px';
+      return 'position: fixed; top: 10px; right: 10px;';
     case 'top-left':
-      return 'position: fixed; top: 10px; left: 10px';
+      return 'position: fixed; top: 10px; left: 10px;';
     case 'top-center':
       return 'position: fixed; top: 10px; left: 50%;';
     case 'bottom-right':
-      return 'position: fixed; bottom: 10px; right: 10px';
+      return 'position: fixed; bottom: 10px; right: 10px;';
     case 'bottom-left':
-      return 'position: fixed; bottom: 10px; left: 10px';
+      return 'position: fixed; bottom: 10px; left: 10px;';
     case 'bottom-center':
-      return 'position: fixed; bottom: 10px; left: 50%';
+      return 'position: fixed; bottom: 10px; left: 50%;';
     default:
-      return 'position: fixed; top: 10px; right: 10px';
+      return 'position: fixed; top: 10px; right: 10px;';
   }
 };
