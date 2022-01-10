c83520d1525a767b9e8f292711ea9eff0c03ed54
C5m7b4 <mbedingfield@dcrpos.com>
2022-01-10 10:41:18 -0600

    add minified css

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,9 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
-import postcss from 'rollup-plugin-postcss';
 import { terser } from 'rollup-plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import scss from 'rollup-plugin-scss';
 
 const packageJson = require('./package.json');
 
@@ -31,7 +31,12 @@ export default [
         tsconfig: './tsconfig.json',
         exclude: ['**/tests', '**/*.test.ts', '**/*.test.tsx'],
       }),
-      postcss(),
+      scss({
+        output: './dist/css/style.min.css',
+        failOnError: true,
+        runtime: require('sass'),
+        outputStyle: 'compressed',
+      }),
       terser(),
     ],
   },
