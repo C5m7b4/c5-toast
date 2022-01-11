f28eede2402ebb176f3114cd2e3f3e6c90a23fc7
C5m7b4 <mbedingfield@dcrpos.com>
2022-01-11 08:41:16 -0600

    refactor toast

--- src/core/toast.ts
+++ src/core/toast.ts
@@ -2,22 +2,37 @@ import { ToastManager as toastManager, Event } from './toastManager';
 import { ToastOptions, TypeOptions } from '../types';
 import { TYPE } from '../utils';
 import { ToastContent } from '../types';
-import { Toast } from '../interfaces';
 
-const createToastByType =
-  (type: TypeOptions) => (content: ToastContent, options?: ToastOptions) =>
-    toast({ content, type, options });
+// const createToastByType =
+//   (type: TypeOptions) => (content: ToastContent, options?: ToastOptions) =>
+//     toast({ content, type, options });
 
-const toast = (props: Toast) => {
-  const { content, type, options } = props;
-  console.log(`creating toast with content: ${content} and type: ${type}`);
-  toastManager.publish(Event.Show, { content, type, options });
+function toast(
+  content: ToastContent,
+  type?: TypeOptions,
+  options?: ToastOptions
+) {
+  if (options) {
+    toastManager.publish(Event.Show, { content, type, options });
+  } else {
+    toastManager.publish(Event.Show, { content });
+  }
+}
+
+toast.success = function (content: ToastContent, options?: ToastOptions) {
+  toast(content, TYPE.SUCCESS, options);
 };
 
-toast.success = createToastByType(TYPE.SUCCESS);
-toast.warning = createToastByType(TYPE.WARNING);
-toast.error = createToastByType(TYPE.ERROR);
-toast.info = createToastByType(TYPE.INFO);
-toast.dark = createToastByType(TYPE.DARK);
+// const toast = (props: CoreToastProps) => {
+//   const { content, type, options } = props;
+//   console.log(`creating toast with content: ${content} and type: ${type}`);
+//   toastManager.publish(Event.Show, { content, type, options });
+// };
+
+// toast.success = createToastByType(TYPE.SUCCESS);
+// toast.warning = createToastByType(TYPE.WARNING);
+// toast.error = createToastByType(TYPE.ERROR);
+// toast.info = createToastByType(TYPE.INFO);
+// toast.dark = createToastByType(TYPE.DARK);
 
 export { toast };
