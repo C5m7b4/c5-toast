cbd6396113f003f86b61b7c1737bbcc249da6b1f
C5m7b4 <mbedingfield@dcrpos.com>
2022-01-12 16:58:44 -0600

    add default check

--- example/components/ContainerCode.tsx
+++ example/components/ContainerCode.tsx
@@ -15,7 +15,6 @@ function getProp<L, R>(prop: L, value: R) {
 }
 
 export interface ContainerCodeProps extends Partial<ToastContainerProps> {
-  isDefaultProps: boolean;
   autoCloseDelay: number;
   animation: AnimationTypes;
 }
@@ -27,34 +26,60 @@ export const ContainerCode: React.FC<ContainerCodeProps> = ({
   autoCloseDelay,
   showIcons,
   animation,
-}) => (
-  <div className="container-code">
-    <h3>Toast Container</h3>
-    <div className="code">
-      <div>
-        <span>{`<`}</span>
-        <span className="code__component">ToastContainer</span>
-      </div>
-      <div>
-        <span className="code__props">position</span>
-        {`="${position}"`}
-      </div>
-      {getProp('showLastOnTop', showLastOnTop)}
-      {getProp('autoClose', autoClose)}
-      {autoClose == true ? (
-        <div>
-          <span className="code__props">autoCloseDelay</span>=&#123;
-          {autoCloseDelay}&#125;
-        </div>
-      ) : null}
-      {getProp('showIcons', showIcons)}
-      <div>
-        <span className="code__props">animation</span>
-        {`={'${animation}'}`}
-      </div>
-      <div>
-        <span>{`/>`}</span>
-      </div>
+}) => {
+  let isDefault = false;
+  if (
+    autoClose === true &&
+    animation === 'slide' &&
+    showLastOnTop === false &&
+    position === 'top-right' &&
+    showIcons === false
+  ) {
+    isDefault = true;
+  }
+  return (
+    <div className="container-code">
+      <h3>Toast Container</h3>
+      {isDefault ? (
+        <React.Fragment>
+          <div className="code">
+            <div>
+              <span>{`<`}</span>
+              <span className="code__component">ToastContainer </span>
+              <span>{`/>`}</span>
+            </div>
+          </div>
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          <div className="code">
+            <div>
+              <span>{`<`}</span>
+              <span className="code__component">ToastContainer</span>
+            </div>
+            <div>
+              <span className="code__props">position</span>
+              {`="${position}"`}
+            </div>
+            {getProp('showLastOnTop', showLastOnTop)}
+            {getProp('autoClose', autoClose)}
+            {autoClose == true ? (
+              <div>
+                <span className="code__props">autoCloseDelay</span>=&#123;
+                {autoCloseDelay}&#125;
+              </div>
+            ) : null}
+            {getProp('showIcons', showIcons)}
+            <div>
+              <span className="code__props">animation</span>
+              {`={'${animation}'}`}
+            </div>
+            <div>
+              <span>{`/>`}</span>
+            </div>
+          </div>
+        </React.Fragment>
+      )}
     </div>
-  </div>
-);
+  );
+};
