372bd86f68f6c14116844c9e18ccc5d1db669620
C5m7b4 <mbedingfield@dcrpos.com>
2022-01-12 10:20:16 -0600

    remove debugger

--- src/core/toast.ts
+++ src/core/toast.ts
@@ -8,7 +8,6 @@ function dispatchToast(
   content: ToastContent,
   options: NotValidatedToastProps
 ): Id {
-  debugger;
   toastManager.publish(Event.Show, content, options);
   return options.toastId;
 }
