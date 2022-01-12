6cc2bd732ba2fa630b8b79290f8a128eabda7122
C5m7b4 <mbedingfield@dcrpos.com>
2022-01-12 09:31:38 -0600

    refactor publish

--- src/core/toast.ts
+++ src/core/toast.ts
@@ -1,26 +1,38 @@
-import { ToastManager as toastManager, Event } from './toastManager';
-import { ToastOptions, TypeOptions } from '../types';
+import { toastManager, Event } from './toastManager';
+import { NotValidatedToastProps, ToastOptions } from '../types';
 import { TYPE } from '../utils';
-import { ToastContent } from '../types';
+import { ToastContent, Id } from '../types';
+import { generateToastId } from '../utils';
+
+function dispatchToast(
+  content: ToastContent,
+  options: NotValidatedToastProps
+): Id {
+  debugger;
+  toastManager.publish(Event.Show, content, options);
+  return options.toastId;
+}
 
 const createToastByType =
   (type: string) => (content: ToastContent, options?: ToastOptions) =>
-    toastManager.publish(Event.Show, mergeOptions(type, options));
+    dispatchToast(content, mergeOptions(type, options));
 
 function mergeOptions(type: string, options?: ToastOptions) {
   return {
     ...options,
     type: (options && options.type) || type,
-  };
+    toastId: generateToastId(),
+  } as NotValidatedToastProps;
 }
 
 const toast = (content: ToastContent, options?: ToastOptions) =>
-  toastManager.publish(Event.Show, mergeOptions(TYPE.DEFAULT, options));
+  dispatchToast(content, mergeOptions(TYPE.DEFAULT, options));
 
 toast.success = createToastByType(TYPE.SUCCESS);
 toast.info = createToastByType(TYPE.INFO);
 toast.warning = createToastByType(TYPE.WARNING);
 toast.error = createToastByType(TYPE.ERROR);
+toast.dark = createToastByType(TYPE.DARK);
 toast.warn = toast.warning;
 
 export { toast };
